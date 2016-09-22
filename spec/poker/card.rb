b8d9565fc2696da6b477893ea08e7ea1c9df4b7d
Dawid <d.kulpa@selleo.com>
2016-09-22 12:49:39 +0200

    gived values to the cards to compare them

--- spec/poker/card.rb
+++ spec/poker/card.rb
@@ -1,4 +1,5 @@
 class Card
+  attr_accessor :value
 
   CARD_NAMES = %w(2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC
                   2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD
@@ -11,13 +12,34 @@ class Card
     else
       raise PokerError::InvalidCardError
     end
+
+    if %w(T J Q K A).include?(@name[0])
+      set_value
+    else
+      @value = @name[0].to_i
+    end
   end
 
   def > card
-    true
+    value > card.value
   end
 
   def < card
-    true
+    value < card.value
+  end
+
+  private
+  def set_value
+    if @name[0] == 'T'
+      @value = 10
+    elsif @name[0] == 'J'
+      @value = 11
+    elsif @name[0] == 'Q'
+      @value = 12
+    elsif @name[0] == 'K'
+      @value = 13
+    elsif @name[0] == 'A'
+      @value = 14
+    end
   end
 end
