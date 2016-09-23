98e2cf24bf983f4005bac9b403bef2ef505ee89d
Dawid <d.kulpa@selleo.com>
2016-09-23 11:32:14 +0200

    divided line to left and right hand

--- spec/poker/poker_table.rb
+++ spec/poker/poker_table.rb
@@ -1,19 +1,28 @@
 class PokerTable
+
   def initialize(line)
-    if line == 'AS 7H AH 7S QC 6H 2D TD JD AS'
-      raise PokerError::DuplicatedCardError
-    else
+    if line.split.uniq == line.split
       @line = line
+    else
+      raise PokerError::DuplicatedCardError
     end
+
+    divide_line
   end
 
-  def strongest_hand
-    if @line == '6D 7H AH 7S QC 6H 2D TD JD AS' || @line == 'AD KH JC 5D 6D AC KC JD 6C 2D' || @line == 'AD AH AC AS 7D KD KC KS KH 7C'
-      'left'
-    elsif @line == 'JH 5D 7H TC JS JD JC TS 5S 7S'
-      'tie'
-    elsif @line == '2H 8C AD TH 6H QD KD 9H 6S 6C'
-      'right'
+  def divide_line
+    @left_hand = @line.split.each_slice(5).to_a[0]
+    @right_hand = @line.split.each_slice(5).to_a[1]
+  end
+
+  private
+  def create_hand(card_codes)
+    Hand.new(get_cards(card_codes))
+  end
+
+  def get_cards(card_codes)
+    card_codes.map do |card_code|
+      Card.new(card_code)
     end
   end
 end
